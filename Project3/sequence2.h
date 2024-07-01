6f75a9d6c5e34af37b4166076e3fe875e9421f2f
Hamim <hamimc232@gmail.com>
2024-07-01 13:53:04 -0400

    Update sequence2.h

--- Project3/sequence2.h
+++ Project3/sequence2.h
@@ -106,10 +106,12 @@ public:
   typedef double value_type;
   typedef std::size_t size_type;
   static const size_type DEFAULT_CAPACITY = 30;
+
   // CONSTRUCTORS and DESTRUCTOR
   sequence(size_type initial_capacity = DEFAULT_CAPACITY);
   sequence(const sequence &source);
   ~sequence();
+
   // MODIFICATION MEMBER FUNCTIONS
   void resize(size_type new_capacity);
   void start();
@@ -118,6 +120,7 @@ public:
   void attach(const value_type &entry);
   void remove_current();
   void operator=(const sequence &source);
+
   // CONSTANT MEMBER FUNCTIONS
   size_type size() const;
   bool is_item() const;
